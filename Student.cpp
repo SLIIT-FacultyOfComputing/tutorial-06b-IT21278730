e37cc998d8b08da0230c5a4336cc9e009f6b62ec
niyumi <99313696+IT21278730@users.noreply.github.com>
2022-03-18 03:21:24 +0000

    student.cpp completed

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,20 @@
 #include "Student.h"
 #include <iostream>
+#include<cstring>
+
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int no, char name[]) {
+  studentID = no;
+  strcpy(studentName , name);
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display()
+{
+  cout << "Student ID : " << studentID << endl;
   
+  cout << "Student name : " << studentName << endl;
+ 
 }
